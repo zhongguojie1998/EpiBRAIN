722ba33b29438322cdc24632352467d01a2af7ff
DiLiu <dl3738@cumc.columbia.edu>
2025-06-06 21:49:43 +0000

    modified:   launchjob.py

--- launchjob.py
+++ launchjob.py
@@ -24,13 +24,16 @@ def validate_machine(ctx, param, value):
 
 @click.command()
 @click.option("-e", "--exp_name", type=str, required=True, help="The exp name")
+@click.option("-c", "--config", type=str, required=True, default="default", help="The config we want to use")
 @click.option(
     "-m", "--machine", multiple=True, required=True, callback=validate_machine, help="The node(s) to use"
 )
-@click.option("-c", "--load_checkpoint", help="Whether to load the chk and continue training")
+@click.option("--load_checkpoint", help="Whether to load the chk and continue training")
 @click.option("--nproc-per-node", default=4, help="GPU available per machine")
 @click.option("--master-port", default=1234, help="The master node port")
-def launch_distributed(exp_name: str, machine: List[str], load_checkpoint: str, nproc_per_node: int, master_port: int):
+def launch_distributed(
+    exp_name: str, config: str, machine: List[str], load_checkpoint: str, nproc_per_node: int, master_port: int
+):
 
     if not machine:
         raise click.UsageError("At least need one machine")
@@ -48,6 +51,8 @@ def launch_distributed(exp_name: str, machine: List[str], load_checkpoint: str, 
             f"--master_addr={master_addr}",
             f"--master_port={master_port}",
             "Model/train.py",
+            "-c",
+            config,
             "-t",
             "-x",
             f"logging=debug",
