48e9ed5eb45d49ea953b86649ac75ba2aa8d9668
DiLiu <dl3738@cumc.columbia.edu>
2025-07-11 01:22:43 -0400

    Add feature: interpret in multiprocess & bug fix: NAN isue & change input style

--- Analysis/02_motif_interpretation.py
+++ Analysis/02_motif_interpretation.py
@@ -1,6 +1,9 @@
 import logging
+import math
+import multiprocessing as mp
 import os
 import sys
+import warnings
 from pathlib import Path
 
 import click
@@ -10,17 +13,16 @@ import pandas as pd
 import torch
 from captum.attr import DeepLift, IntegratedGradients
 from modisco.visualization import viz_sequence
-from peft import LoraConfig, get_peft_model
 from tqdm import tqdm
 
 ROOT = Path(__file__).parent.parent
 sys.path.append(str(ROOT / "Model"))
 os.chdir(ROOT)
+warnings.filterwarnings("ignore")
 
 from data.data_utils import STD_CHR
 from data.tokenizer import FastaInterval, str_to_one_hot
-from model.pytorch_borzoi_model import Borzoi
-from model.pytorch_borzoi_utils import safe_state_dict_loader
+from model.model_utils import setup_model
 from utils.config import load_config
 from utils.logging import BaseLogger
 
@@ -32,92 +34,70 @@ def which_bins(s_idx: int, e_idx: int, window_size: int):
     return np.array(range(bin_start, bin_end + 1))
 
 
-@click.command()
-@click.option("--rank", required=True, type=str, default="cuda", help="Device to use")
-@click.option("--exp_name", "-e", required=True, type=str)
-@click.option("--chk", required=True, type=str)
-@click.option("--trial", "-t", required=True, type=str)
-@click.option("--peak_file", "-p", required=True, type=str)
-@click.option("--config", "-c", required=True, type=str, default="default")
-@click.option("--config_base", required=True, type=str, default="./Config")
-@click.option("--chk_base", required=True, type=str, default="./Chk")
-@click.option("--res_base", required=True, type=str, default="./Res")
-def main(rank, exp_name, chk, trial, peak_file, config, config_base, chk_base, res_base):
-    CONFIG_DIR = os.path.abspath(config_base)
-    CHK_BASE = os.path.abspath(chk_base)
-    RES_BASE = os.path.abspath(res_base)
+def process_region_chunk(args):
+    (
+        exp_name,
+        chk,
+        region_chunk_data,
+        baseline_type,
+        config_path,
+        checkpoint_path,
+        res_base,
+        device,
+        force_restart,
+    ) = args
 
-    os.makedirs(f"{RES_BASE}/{exp_name}/analysis_{chk}/plot/interp", exist_ok=True)
-    os.makedirs(f"{RES_BASE}/{exp_name}/analysis_{chk}/raw_data/interp", exist_ok=True)
+    # Load config
+    myconfig = load_config(config_name=config_path)
+    logger = BaseLogger(name=f"Interpretation-{device}", level=logging.INFO)
 
-    # read in the config
-    myconfig = load_config(CONFIG_DIR, config, overrides=[])
-    logger = BaseLogger(name="Interpretation", level=logging.DEBUG)
-
-    # get label
+    # Get label information
     label_meta = pd.read_csv(f"{myconfig.data.storage_path}/label_meta.csv", index_col=1)
-    trial_dim = label_meta.loc[trial, "dim"]
-    peak_df = pd.read_csv(peak_file, header=None, sep="\t")
-    assert peak_file.split("/")[-1].split(".")[0] + "_" + peak_file.split("/")[-1].split("_")[2] == trial
 
-    # get model
-    checkpoint = torch.load(f"{CHK_BASE}/{exp_name}/chk_epoch_{chk}.pt", map_location="cpu")
-
-    model = Borzoi.from_hparams(**myconfig.model)
-    if myconfig.training.finetune:
-        # load the pretrained state dict
-        pretrained_model = Borzoi.from_pretrained(myconfig.model.model_name)
-        org_model_state_dict = model.state_dict()
-        updated_model_state_dict = safe_state_dict_loader(
-            org_model_state_dict=org_model_state_dict,
-            load_model_state_dict=pretrained_model.state_dict(),
-            logger=logger,
-        )
-        org_model_state_dict.update(updated_model_state_dict)
-        model.load_state_dict(org_model_state_dict)
-
-        # initialize the finetune model
-        if myconfig.model.finetune_method == "lora":
-            logger.info("LORA Finetune")
-            finetune_config = LoraConfig(**myconfig.model.finetune_param)
-            model = get_peft_model(model, finetune_config)
-        elif myconfig.model.finetune_method == "finetune_layers":
-            pass
-        else:
-            logger.error(f"Finetune method {myconfig.model.finetune_method} is not implemented yet.")
-            exit(1)
-
+    # Setup model
+    checkpoint = torch.load(checkpoint_path, map_location="cpu")
+    model = setup_model(myconfig, logger)
     model.load_state_dict(checkpoint["model_state_dict"])
-    dl_model = DeepLift(model.eval().to(rank), multiply_by_inputs=True)
+    dl_model = DeepLift(model.eval().to(device), multiply_by_inputs=True, eps=1e-7)
 
-    # get data
-    ## baseline
-    np.random.seed(myconfig.training.seed)
-    baseline_seq = "".join(
-        np.random.choice(["A", "T", "C", "G"], size=myconfig.data.context_length, p=[0.3, 0.3, 0.2, 0.2])
-    )
+    # Setup baseline
+    if baseline_type == "random":
+        np.random.seed(myconfig.training.seed)
+        baseline_seq = "".join(
+            np.random.choice(["A", "T", "C", "G"], size=myconfig.data.context_length, p=[0.3, 0.3, 0.2, 0.2])
+        )
+    elif baseline_type == "all_zero":
+        baseline_seq = "N" * myconfig.data.context_length
     baseline_seq_onehot = str_to_one_hot(baseline_seq)
 
-    ## test
+    # Setup tokenizer
     dna_tokenizer = FastaInterval(
         fasta_file=os.path.abspath(myconfig.data.refer_genom), context_length=myconfig.data.context_length
     )
 
-    for idx in tqdm(range(len(peak_df))):
-        chr_name, start, end = peak_df.iloc[idx, [0, 1, 2]]
+    # Process each region in the chunk
+    for idx in range(len(region_chunk_data)):
+        chr_name, start, end, _, trial = region_chunk_data.iloc[idx, [0, 1, 2, 3, 4]]
+        identifier = f"{chr_name}_{start}_{end}_{trial}"
+
+        try:
+            trial_dim = label_meta.loc[trial, "dim"]
+        except:
+            logger.warning(f"{trial} cannot be found in label meta, skip")
+            continue
+
         if not chr_name in STD_CHR:
             continue
 
         token_dict = dna_tokenizer(
-                chr_name=chr_name, start=start, end=end, return_augs=False, return_rela_idx=True
-            )
+            chr_name=chr_name, start=start, end=end, return_augs=False, return_rela_idx=True
+        )
 
         s_idx, e_idx = token_dict["rela_idx"]
         test_seq_onehot = token_dict["one_hot"]
         test_seq_onehot.requires_grad = True
 
-        # get the interested bin, given the raw idx
-        # change the bin index to relative index, given the trimmed window
+        # Get the interested bin, given the raw idx
         bin_range = which_bins(s_idx, e_idx, myconfig.data.preprocess.window_size)
         trim = (
             myconfig.data.context_length // myconfig.data.preprocess.window_size
@@ -128,35 +108,153 @@ def main(rank, exp_name, chk, trial, peak_file, config, config_base, chk_base, r
         assert bin_range.min() >= 0
         assert bin_range.max() <= myconfig.data.preprocess.n_window
 
-        if not os.path.exists(f"{RES_BASE}/{exp_name}/analysis_{chk}/raw_data/interp/{chr_name}_{start}_{end}.pt"):
-            
+        if (
+            not os.path.exists(f"{res_base}/{exp_name}/analysis_{chk}/raw_data/interp/{identifier}.pt")
+            or force_restart
+        ):
             all_attribution = []
+            nan_occur = False
             for bin in bin_range:
                 attribution = dl_model.attribute(
-                    inputs=test_seq_onehot.unsqueeze(0).permute(0, 2, 1).to(rank),
-                    baselines=baseline_seq_onehot.unsqueeze(0).permute(0, 2, 1).to(rank),
+                    inputs=test_seq_onehot.unsqueeze(0).permute(0, 2, 1).to(device),
+                    baselines=baseline_seq_onehot.unsqueeze(0).permute(0, 2, 1).to(device),
                     target=(trial_dim, bin),
                 )
+                if not torch.isfinite(attribution).all():
+                    logger.warning(f"NAN occur in {identifier}, bin num {bin}")
+                    nan_occur = True
                 all_attribution.append(attribution.squeeze(0).detach().cpu())
             all_attribution = torch.stack(all_attribution)
 
             torch.save(
                 all_attribution,
-                f"{RES_BASE}/{exp_name}/analysis_{chk}/raw_data/interp/{chr_name}_{start}_{end}.pt",
+                f"{res_base}/{exp_name}/analysis_{chk}/raw_data/interp/{identifier}.pt",
             )
         else:
-            all_attribution = torch.load(
-                f"{RES_BASE}/{exp_name}/analysis_{chk}/raw_data/interp/{chr_name}_{start}_{end}.pt"
-            )
+            all_attribution = torch.load(f"{res_base}/{exp_name}/analysis_{chk}/raw_data/interp/{identifier}.pt")
+            if not torch.isfinite(all_attribution).all():
+                logger.warning(f"NAN occur in {identifier}")
+                nan_occur = True
+            else:
+                nan_occur = False
 
-        # plot
+        if nan_occur:
+            logger.warning(f"NAN occur in {identifier}. Skip plotting")
+            continue
+
+        # Plot
         viz_sequence.plot_weights(all_attribution.mean(dim=0)[:, s_idx:e_idx].T, subticks_frequency=20)
         plt.savefig(
-            f"{RES_BASE}/{exp_name}/analysis_{chk}/plot/interp/{chr_name}_{start}_{end}.png",
+            f"{res_base}/{exp_name}/analysis_{chk}/plot/interp/{identifier}.png",
             dpi=300,
             bbox_inches="tight",
         )
         plt.close()
+
+
+@click.command()
+@click.option("--region_bed", "-f", required=True, type=str, help="Path to a file with all regions to test")
+@click.option("--exp_name", "-e", required=True, type=str)
+@click.option("--chk", required=True, type=str)
+@click.option(
+    "--baseline",
+    "-b",
+    required=True,
+    type=click.Choice(["random", "all_zero"], case_sensitive=False),
+    default="random",
+)
+@click.option("--log_base", required=True, type=str, default="./logs")
+@click.option("--chk_base", required=True, type=str, default="./Chk")
+@click.option("--res_base", required=True, type=str, default="./Res")
+@click.option("--force_restart", is_flag=True)
+@click.option(
+    "--processor",
+    required=True,
+    type=click.Choice(["cpu", "gpu"], case_sensitive=False),
+    default="gpu",
+)
+@click.option("--num_processes", type=int, default=1, help="Number of subprocess to use for parallel processing")
+def main(
+    region_bed, exp_name, chk, baseline, log_base, chk_base, res_base, force_restart, processor, num_processes
+):
+    LOG_BASE = os.path.abspath(log_base)
+    CHK_BASE = os.path.abspath(chk_base)
+    RES_BASE = os.path.abspath(res_base)
+
+    os.makedirs(f"{RES_BASE}/{exp_name}/analysis_{chk}/plot/interp", exist_ok=True)
+    os.makedirs(f"{RES_BASE}/{exp_name}/analysis_{chk}/raw_data/interp", exist_ok=True)
+
+    logger = BaseLogger(name="Interpretation", level=logging.DEBUG)
+
+    # read region data
+    region_df = pd.read_csv(region_bed, header=None, sep="\t")
+    logger.info(f"Total regions to process: {len(region_df)}")
+
+    # Check availability
+    if processor == "gpu":
+        available_devices = torch.cuda.device_count()
+        if available_devices == 0:
+            logger.warning("No GPU found. Using CPU")
+            processor = "cpu"
+
+    if processor == "cpu":
+        available_devices = mp.cpu_count()
+
+    if num_processes > available_devices:
+        logger.warning(
+            f"Requested {num_processes} {processor} but only {available_devices} available. Using {available_devices} {processor}."
+        )
+        num_processes = available_devices
+
+    # Multi-Process parallel processing
+    logger.info(f"Processing with {num_processes} processes")
+
+    # Split regions into chunks for parallel processing
+    chunks = []
+    n = len(region_df)
+
+    if processor == "gpu":
+        base = n // num_processes
+        extra = n % num_processes
+        start = 0
+        for i in range(num_processes):
+            size = base + (1 if i < extra else 0)
+            end = start + size
+            if start < end:
+                chunk = region_df.iloc[start:end].copy()
+                chunks.append(chunk)
+            start = end
+    elif processor == "cpu":
+        for i in range(n):
+            chunk = region_df.iloc[i : i + 1].copy()
+            chunks.append(chunk)
+
+    logger.info(f"Split {len(region_df)} regions into {len(chunks)} chunks")
+    for chunk in chunks:
+        logger.debug(chunk)
+
+    # Prepare arguments for each process
+    process_args = []
+    for process_id, chunk in enumerate(chunks):
+        args = (
+            exp_name,
+            chk,
+            chunk,
+            baseline,
+            f"{LOG_BASE}/{exp_name}/overall_setting.yaml",
+            f"{CHK_BASE}/{exp_name}/chk_epoch_{chk}.pt",
+            RES_BASE,
+            f"cuda:{process_id}" if processor == "gpu" else "cpu",
+            force_restart,
+        )
+        process_args.append(args)
+
+    # Run parallel processing
+    logger.info("Starting processing...")
+    if processor == "gpu":
+        mp.set_start_method("spawn", force=True)
+    with mp.Pool(processes=num_processes) as pool:
+        pool.map(process_region_chunk, process_args)
 
 
 if __name__ == "__main__":
