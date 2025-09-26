d743b5713cc4062913720b280a0dd98a2cd2e83a
DiLiu <dl3738@cumc.columbia.edu>
2025-09-26 15:48:56 +0000

    Fix import issue

--- Model/model/borzoi_pytorch/__init__.py
+++ Model/model/borzoi_pytorch/__init__.py
@@ -1,2 +1,8 @@
-from borzoi_pytorch.pytorch_borzoi_model import Borzoi, AnnotatedBorzoi
-from borzoi_pytorch.gene_utils import Transcriptome
+import sys
+from pathlib import Path
+current = Path(__file__)
+sys.path.append(str(current.parent.parent))
+
+
+from borzoi_pytorch.pytorch_borzoi_model import Borzoi
+# from borzoi_pytorch.gene_utils import Transcriptome
